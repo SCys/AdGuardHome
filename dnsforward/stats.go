b3d0d2b5f4afe6594b4118d3b2244f21591fe841
SCys <me@iscys.com>
2020-06-07 13:34:03 +0800

    * 增加运行时插入的 Hook

--- dnsforward/stats.go
+++ dnsforward/stats.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/AdguardTeam/AdGuardHome/querylog"
+	"github.com/AdguardTeam/AdGuardHome/worker"
 	"github.com/miekg/dns"
 
 	"github.com/AdguardTeam/AdGuardHome/dnsfilter"
@@ -43,6 +44,8 @@ func processQueryLogsAndStats(ctx *dnsContext) int {
 			p.Upstream = d.Upstream.Address()
 		}
 		s.queryLog.Add(p)
+
+		worker.ProcessDNSResult(ctx.result, d.Res)
 	}
 
 	s.updateStats(d, elapsed, *ctx.result)
